6ec3f405a2630af7f8b3ec870f79a6982af21d01
ghillairet <g.hillairet@gmail.com>
2015-10-31 13:34:05 +0100

    Add documentation for the score generator.

--- eclipse/org.emfjson.sample.xcore.editor/src-gen/org/emfjson/sample/model/presentation/ModelEditor.java
+++ eclipse/org.emfjson.sample.xcore.editor/src-gen/org/emfjson/sample/model/presentation/ModelEditor.java
@@ -949,7 +949,7 @@ public class ModelEditor
 	 * @generated
 	 */
 	public void createModel() {
-		URI resourceURI = EditUIUtil.getURI(getEditorInput());
+		URI resourceURI = EditUIUtil.getURI(getEditorInput(), editingDomain.getResourceSet().getURIConverter());
 		Exception exception = null;
 		Resource resource = null;
 		try {
@@ -977,10 +977,11 @@ public class ModelEditor
 	 * @generated
 	 */
 	public Diagnostic analyzeResourceProblems(Resource resource, Exception exception) {
-		if (!resource.getErrors().isEmpty() || !resource.getWarnings().isEmpty()) {
+		boolean hasErrors = !resource.getErrors().isEmpty();
+		if (hasErrors || !resource.getWarnings().isEmpty()) {
 			BasicDiagnostic basicDiagnostic =
 				new BasicDiagnostic
-					(Diagnostic.ERROR,
+					(hasErrors ? Diagnostic.ERROR : Diagnostic.WARNING,
 					 "org.emfjson.sample.xcore.editor",
 					 0,
 					 getString("_UI_CreateModelError_message", resource.getURI()),
